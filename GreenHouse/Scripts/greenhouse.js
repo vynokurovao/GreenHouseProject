d267ec0608fdab4a718c8408c9c7c4a11193523d
IvaLev <levvania@mail.ru>
2015-09-11 19:04:48 +0300

    fixing; get date

--- GreenHouse/Scripts/greenhouse.js
+++ GreenHouse/Scripts/greenhouse.js
@@ -6,9 +6,16 @@ $(function () {
     $('#datetimepicker12').datepicker({
         inline: true,
         sideBySide: true,
-        language: 'ru',
-        todayBtn: true
+        language: 'ru'
     });
+
+    $('#datetimepicker12').on("changeDate", function (e) {
+        var day = e.date.getUTCDate() + 1;
+        var month = e.date.getMonth() + 1;
+        var year = e.date.getFullYear();
+        $('#forDate').html('на ' + day + '.' + month + '.' + year);
+    });
+
 });
 
 (function ($) {
@@ -21,7 +28,8 @@ $(function () {
             on_screen_pic = 9;
             slider_diff = 60;
             var x = $("#cont").position();
-            if (Math.abs(x.left) + slider_diff * on_screen_pic < $("#slider").width()) {
+            if (Math.abs(x.left) + slider_diff * on_screen_pic < $("#slider").width())
+            {
                 newcoord = (x.left - slider_diff);
                 if (Math.abs(newcoord) + slider_diff >= $("#slider").width())
                     newcoord = -$("#slider").width() + slider_diff;
@@ -40,20 +48,24 @@ $(function () {
             }
         },
 
+        btnToday_Click: function () {
+            $('#datetimepicker12').datepicker('setDate', new Date('2015-09-11'));
+        },
+
         btnToggleCalendar_Click: function (elem) {
             element = $(elem);
             $('#calendarContainer').toggle();
 
-            if ($('#table-container').hasClass('col-xs-6')) {
-                $('#lenta').width(1000);
+            if ($('#table-container').hasClass('col-xs-7')) {
+                $('#lenta').width(875);
                 $("#cont").animate({ left: "0px" }, 150);
-                $('#table-container').removeClass('col-xs-6').addClass('col-xs-10');
+                $('#table-container').removeClass('col-xs-7').addClass('col-xs-10');
                 element.html('Показать');
 
             }
             else if ($('#table-container').hasClass('col-xs-10')) {
                 $('#lenta').width(600);
-                $('#table-container').removeClass('col-xs-10').addClass('col-xs-6');
+                $('#table-container').removeClass('col-xs-10').addClass('col-xs-7');
                 element.html('Скрыть');
             }
         },
