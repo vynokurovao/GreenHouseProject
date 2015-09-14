23ff2529d81d61ae8b84398c7abf015fa5aeccd7
Oksana Vynokurova <oksanka.vynokurova@gmail.com>
2015-09-15 00:51:06 +0300

    Added LoginController, LoginModel and LoginView

---
+++ GreenHouse/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GreenHouse.Models;
+
+namespace GreenHouse.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView();
+            }
+
+            return View(new LoginModel());
+        }
+
+        [HttpPost]
+        public ActionResult Add(LoginModel userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                using (Entities db = new Entities())
+                {
+                    IQueryable<User> users = db.User
+                        .Where(user => user.Email.Equals(userInfo.Email) && user.Password.Equals(userInfo.Password));
+
+                    if (users.AsEnumerable().Count() == 0)
+                    {
+                        ModelState.AddModelError("", "Данные введены не верно");
+                        return PartialView("Create", userInfo);
+                    }
+                    else
+                    {
+                        return PartialView();
+                    }
+                }
+            }
+      
+            return PartialView("Create", userInfo);
+        }
+    }
+}
