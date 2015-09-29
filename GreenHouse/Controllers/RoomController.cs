1585449bef4de722fd08403c6e9e70e9ad521ec5
IvaLev <levvania@mail.ru>
2015-09-29 21:17:09 +0300

    reservation for a day

--- GreenHouse/Controllers/RoomController.cs
+++ GreenHouse/Controllers/RoomController.cs
@@ -60,5 +60,66 @@ namespace GreenHouse.Controllers
 
             return View("Room", reservManager);
         }
+
+        public int IsCanBlockOnPeriod(ReservationAuditoriumDay model)
+        {
+            int result = 1;
+
+            string[] parts = model.date.Split('.');
+
+            int year = int.Parse(parts[2]), month = int.Parse(parts[1]), day = int.Parse(parts[0]);
+
+            DateTime date = new DateTime(year, month, day, DateTime.Now.Hour + 1, 0, 0);
+
+            if (date < DateTime.Now)
+            {
+                return 2; //in the past
+            }
+
+            ReservationManager reservManager = new ReservationManager(date);
+
+            reservManager.Table = new List<List<TD>>();
+
+            if (model.period)
+            {
+                reservManager.Table = reservManager.GetDayRoomReservation(date, model.auditorium);
+            }
+            else
+            {
+                reservManager.Table = reservManager.GetWeekReservation(date, model.auditorium);
+            }
+
+            foreach (List<TD> tr in reservManager.Table)
+            {
+                foreach (TD td in tr)
+                {
+                    if (td.ReservationId > 0)
+                    {
+                        if (td.FinishDate > DateTime.Now || td.StartDate > DateTime.Now)
+                        {
+                            return 0; //reservation is exist
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        //public DateTime GetLastDayWeek(Date _date)
+        //{
+        //    DateTime date = new DateTime(_date.year, _date.month, _date.day, 0, 0, 0);
+
+        //    DayOfWeek dayOfWeek = date.DayOfWeek;
+
+        //    DateTime startdate = date;
+
+        //    startdate = startdate.Subtract(new TimeSpan((int)dayOfWeek - 1, 0, 0, 0));
+
+        //    startdate.AddDays(7);
+
+        //    return startdate;
+        //}
+
     }
 }
--- GreenHouse/Models/NewResevation.cs
+++ GreenHouse/Models/NewResevation.cs
@@ -8,11 +8,16 @@ namespace GreenHouse.Models
     public class NewResevation
     {
         public int auditorium { get; set; }
+        public string auditorium_name { get; set; }
         public string purpose { get; set; }
         public int year { get; set; }
         public int month { get; set; }
         public int day { get; set; }
         public int hour { get; set; }
+        public int finish_year { get; set; }
+        public int finish_month { get; set; }
+        public int finish_day { get; set; }
+        public int finish_hour { get; set; }
         public bool type { get; set; }
         public byte view { get; set; } // 0 - for all auditoiums, 1 - for 1 auditorium, 2 - for a week
     }
